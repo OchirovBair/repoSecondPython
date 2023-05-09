c8f8e8bc16737b28781db1a75687fa458860e7f6
bair <upvolume@mail.ru>
2023-05-09 10:12:50 +0800

    program to mix two colors

--- myFirstPythonProgram.py
+++ myFirstPythonProgram.py
@@ -1,21 +1,16 @@
-x = int(input())
-y = int(input())
-str = input()
-result = 0
-if y == 0 and str == '/':
-    print('На ноль делить нельзя!')
-elif str == '/' or str == '*' or str == '+' or str == '-':
-    if str == '*':
-        result = x * y
-        print(result)
-    elif str == '/':
-        result = x / y
-        print(result)
-    elif str == '+':
-        result = x + y
-        print(result)
-    elif str == '-':
-        result = x - y
-        print(result)
+color1, color2 = input(), input()
+if (color1 == 'красный' or color1 == 'синий' or color1 == 'желтый') and (color2 == 'красный' or color2 == 'синий' or color2 == 'желтый'):
+    if (color1 == 'красный' and color2 == 'синий') or (color1 == 'синий' and color2 == 'красный'):
+        print('фиолетовый')
+    elif (color1 == 'красный' and color2 == 'желтый') or (color1 == 'желтый' and color2 == 'красный'):
+        print('оранжевый')
+    elif (color1 == 'синий' and color2 == 'желтый') or (color1 == 'желтый' and color2 == 'синий'):
+        print('зеленый')
+    elif color1 == 'желтый' and color2 == 'желтый':
+        print('желтый')
+    elif color1 == 'синий' and color2 == 'синий':
+        print('синий')
+    elif color1 == 'красный' and color2 == 'красный':
+        print('красный')
 else:
-    print('Неверная операция')
+    print('ошибка цвета')
